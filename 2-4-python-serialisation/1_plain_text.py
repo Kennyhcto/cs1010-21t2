f666946677e4c984f49ed262cffaa5ddd06a0029
Sim Mautner <s.mautner@unsw.edu.au>
2021-06-25 11:00:12 +1000

    added files for serialisation lecture

---
+++ 2-4-python-serialisation/1_plain_text.py
@@ -0,0 +1,20 @@
+"""
+Reading and Writing Plain Text Files
+"""
+
+
+def get_sample_data():
+    return ["hello", "how are you?", "that's good",
+            "nice to meet you", "goodbye\nI'll see you again"]
+
+
+def write_file(data):
+    pass
+
+
+def read_file():
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ 2-4-python-serialisation/2a_csv_list.py
@@ -0,0 +1,27 @@
+"""
+Comma Separated Values
+
+Tabular (table-like) data can be stored in
+Comma Separated Value (CSV) format.
+"""
+
+import csv
+
+
+def get_sample_data_list():
+    return [['T15A', 'Liz', 21],
+            ['H12A', 'My Name Has A, Comma In It', 21],
+            ['H15A', 'William', 21]]
+
+
+def read_csv_as_list():
+    pass
+
+
+def write_list_as_csv(tutorials):
+    pass
+
+
+if __name__ == "__main__":
+    pass
+
